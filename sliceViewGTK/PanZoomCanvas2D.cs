5638d36f057b90f95384413f805218eff346b424
Ryan Schmidt <rms@rms80.com>
2017-06-10 21:38:25 -0400

    refactored DrawingArea w/ pan-zoom controls out of ViewportCanvasArea into PanZoomCanvas2D

---
+++ sliceViewGTK/PanZoomCanvas2D.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using Gtk;
+using GLib;
+using SkiaSharp;
+using g3;
+
+
+namespace gs
+{
+	public class PanZoomCanvas2D : DrawingArea
+	{
+		public float Zoom = 0.95f;
+
+		// this is a pixel-space translate
+		public Vector2f Translate = Vector2f.Zero;
+
+
+		public PanZoomCanvas2D()
+		{
+			ExposeEvent += OnExpose;
+
+			ButtonPressEvent += OnButtonPressEvent;
+			ButtonReleaseEvent += OnButtonReleaseEvent;
+			MotionNotifyEvent += OnMotionNotifyEvent;
+			ScrollEvent += OnScrollEvent;
+			Events = Gdk.EventMask.ExposureMask | Gdk.EventMask.LeaveNotifyMask |
+						Gdk.EventMask.ButtonPressMask | Gdk.EventMask.ButtonReleaseMask | Gdk.EventMask.PointerMotionMask |
+						Gdk.EventMask.ScrollMask;
+			
+		}
+
+
+
+		protected virtual AxisAlignedBox2d DrawingBounds {
+			get { return new AxisAlignedBox2d(0, 0, 500, 500); }
+		}
+
+		protected virtual void DrawScene(SKCanvas drawCanvas, Func<Vector2d, Vector2f> ViewTransformF) {
+		}
+
+
+
+		void OnExpose(object sender, ExposeEventArgs args)
+		{
+			DrawingArea area = (DrawingArea)sender;
+			Cairo.Context cr = Gdk.CairoHelper.Create(area.GdkWindow);
+
+			int width = area.Allocation.Width;
+			int height = area.Allocation.Height;
+
+			AxisAlignedBox2d bounds = DrawingBounds;
+
+			double sx = (double)width / bounds.Width;
+			double sy = (double)height / bounds.Height;
+
+			float scale = (float)Math.Min(sx, sy);
+
+			// we apply this translate after scaling to pixel coords
+			Vector2f pixC = Zoom * scale * (Vector2f)bounds.Center;
+			Vector2f translate = new Vector2f(width / 2, height / 2) - pixC;
+
+
+			using (var bitmap = new SKBitmap(width, height, SkiaUtil.ColorType(), SKAlphaType.Premul)) {
+				IntPtr len;
+				using (var skSurface = SKSurface.Create(bitmap.Info.Width, bitmap.Info.Height, SkiaUtil.ColorType(), SKAlphaType.Premul, bitmap.GetPixels(out len), bitmap.Info.RowBytes)) {
+					var canvas = skSurface.Canvas;
+
+					// update scene xform
+					Func<Vector2d, Vector2f> ViewTransformF = (pOrig) => {
+						Vector2f pNew = (Vector2f)pOrig - (Vector2f)bounds.Center;
+						pNew = Zoom * scale * pNew;
+						pNew += (Vector2f)pixC;
+						pNew += translate + Zoom * Translate;
+						pNew.y = canvas.ClipBounds.Height - pNew.y;
+						return pNew;
+					};
+
+					DrawScene(canvas, ViewTransformF);
+
+					Cairo.Surface surface = new Cairo.ImageSurface(
+						bitmap.GetPixels(out len),
+						Cairo.Format.Argb32,
+						bitmap.Width, bitmap.Height,
+						bitmap.Width * 4);
+
+					surface.MarkDirty();
+					cr.SetSourceSurface(surface, 0, 0);
+					cr.Paint();
+				}
+			}
+
+			//return true;
+		}
+
+
+
+
+
+		// zoom
+		protected void OnScrollEvent(object o, ScrollEventArgs args)
+		{
+			if (args.Event.Direction == Gdk.ScrollDirection.Up)
+				Zoom *= 1.05f;
+			else
+				Zoom = Math.Max(0.25f, Zoom * (1.0f / 1.05f));
+			QueueDraw();
+		}
+
+
+		// pan support
+		bool left_down = false;
+		Vector2f start_pos = Vector2f.Zero;
+		Vector2f pan_start;
+		protected void OnButtonPressEvent(object o, ButtonPressEventArgs args)
+		{
+			if (args.Event.Button == 1) {
+				left_down = true;
+				start_pos = new Vector2f((float)args.Event.X, (float)args.Event.Y);
+				pan_start = Translate;
+			}
+		}
+		protected void OnButtonReleaseEvent(object o, ButtonReleaseEventArgs args)
+		{
+			if (left_down)
+				left_down = false;
+		}
+		protected void OnMotionNotifyEvent(object o, MotionNotifyEventArgs args)
+		{
+			if (left_down) {
+				Vector2f cur_pos = new Vector2f((float)args.Event.X, (float)args.Event.Y);
+				Vector2f delta = cur_pos - start_pos;
+				delta.y = -delta.y;
+				delta *= 1.0f;  // speed
+				Translate = pan_start + delta / Zoom;
+				QueueDraw();
+			}
+		}
+
+
+		public void Reset()
+		{
+			Zoom = 1.0f;
+			Translate = Vector2f.Zero;
+			QueueDraw();
+		}
+
+
+
+
+
+
+
+	}
+}
--- sliceViewGTK/ViewportCanvasArea.cs
+++ sliceViewGTK/ViewportCanvasArea.cs
@@ -8,17 +8,9 @@ using gs;
 
 namespace SliceViewer 
 {
-	class SliceViewCanvas : DrawingArea
+	class SliceViewCanvas : PanZoomCanvas2D
 	{
-
-
 		public bool ShowOpenEndpoints = true;
-
-		public float Zoom = 0.95f;
-
-		// this is a pixel-space translate
-		public Vector2f Translate = Vector2f.Zero;
-
 
 		public enum NumberModes
 		{
@@ -41,16 +33,6 @@ namespace SliceViewer
 
 		public SliceViewCanvas() 
 		{
-			ExposeEvent += OnExpose;
-
-			ButtonPressEvent += OnButtonPressEvent;
-			ButtonReleaseEvent += OnButtonReleaseEvent;
-			MotionNotifyEvent += OnMotionNotifyEvent;
-			ScrollEvent += OnScrollEvent;
-			Events = Gdk.EventMask.ExposureMask | Gdk.EventMask.LeaveNotifyMask |
-			            Gdk.EventMask.ButtonPressMask | Gdk.EventMask.ButtonReleaseMask | Gdk.EventMask.PointerMotionMask |
-			            Gdk.EventMask.ScrollMask;
-
 			SetPaths(new PathSet());
 			InitializeInternals();
 		}
@@ -107,78 +89,48 @@ namespace SliceViewer
 				return new SKPoint(p.x, p.y);
 			};
 		}
-			
-			
 
 
-		void OnExpose(object sender, ExposeEventArgs args)
-		{
-			DrawingArea area = (DrawingArea) sender;
-			Cairo.Context cr =  Gdk.CairoHelper.Create(area.GdkWindow);
 
-			int width = area.Allocation.Width;
-			int height = area.Allocation.Height;
-
-			//AxisAlignedBox3d bounds3 = Paths.Bounds;
-			AxisAlignedBox3d bounds3 = Paths.ExtrudeBounds;
-			AxisAlignedBox2d bounds = (bounds3 == AxisAlignedBox3d.Empty) ?
-				new AxisAlignedBox2d(0, 0, 500, 500) : 
-				new AxisAlignedBox2d(bounds3.Min.x, bounds3.Min.y, bounds3.Max.x, bounds3.Max.y );
-
-			double sx = (double)width / bounds.Width;
-			double sy = (double)height / bounds.Height;
-
-			float scale = (float)Math.Min(sx, sy);
-
-			// we apply this translate after scaling to pixel coords
-			Vector2f pixC = Zoom * scale * (Vector2f)bounds.Center;
-			Vector2f translate = new Vector2f(width/2, height/2) - pixC;
-
-			using (var bitmap = new SKBitmap(width, height, SkiaUtil.ColorType(), SKAlphaType.Premul))
-			{
-				IntPtr len;
-				using (var skSurface = SKSurface.Create(bitmap.Info.Width, bitmap.Info.Height, SkiaUtil.ColorType(), SKAlphaType.Premul, bitmap.GetPixels(out len), bitmap.Info.RowBytes))
-				{
-					var canvas = skSurface.Canvas;
-					canvas.Clear(SkiaUtil.Color(255, 255, 255, 255));
-
-					// update scene xform
-					SceneXFormF = (pOrig) => {
-						Vector2f pNew = (Vector2f)pOrig - (Vector2f)bounds.Center;
-						pNew = Zoom * scale * pNew;
-						pNew += (Vector2f)pixC;
-						pNew += translate + Zoom*Translate;
-						pNew.y = canvas.ClipBounds.Height - pNew.y;
-						return pNew;
-					};
-
-
-					using (var paint = new SKPaint())
-					{
-						paint.IsAntialias = true;
-
-						paint.StrokeWidth = 1;
-                        paint.Style = SKPaintStyle.Stroke;
-
-						DrawLayerPaths(Paths, canvas, paint);
-
-						if (NumberMode == NumberModes.PathNumbers )
-							DrawPathLabels(Paths, canvas, paint);
-					}
-
-					Cairo.Surface surface = new Cairo.ImageSurface(
-						bitmap.GetPixels(out len),
-						Cairo.Format.Argb32,
-						bitmap.Width, bitmap.Height,
-						bitmap.Width * 4);
-
-					surface.MarkDirty();
-					cr.SetSourceSurface(surface, 0, 0);
-					cr.Paint();
-				}
+		protected override AxisAlignedBox2d DrawingBounds {
+			get {
+				AxisAlignedBox3d bounds3 = Paths.ExtrudeBounds;
+				AxisAlignedBox2d bounds = (bounds3 == AxisAlignedBox3d.Empty) ?
+					new AxisAlignedBox2d(0, 0, 500, 500) :
+					new AxisAlignedBox2d(bounds3.Min.x, bounds3.Min.y, bounds3.Max.x, bounds3.Max.y);
+				return bounds;
 			}
+		}
 
-			//return true;
+
+		protected override void DrawScene(SKCanvas canvas, Func<Vector2d, Vector2f> ViewTransformF)
+		{
+			Random jitter = new Random(313377);
+			Func<Vector2d> jitterF = () => 
+				{ return Vector2d.Zero; };
+				//{ return 0.1 * new Vector2d(jitter.NextDouble(), jitter.NextDouble()); };
+
+			canvas.Clear(SkiaUtil.Color(255, 255, 255, 255));
+
+			// update scene xform
+			SceneXFormF = (pOrig) => {
+				pOrig += jitterF();
+				return ViewTransformF(pOrig);
+			};
+
+
+			using (var paint = new SKPaint())
+			{
+				paint.IsAntialias = true;
+
+				paint.StrokeWidth = 1;
+                paint.Style = SKPaintStyle.Stroke;
+
+				DrawLayerPaths(Paths, canvas, paint);
+
+				if (NumberMode == NumberModes.PathNumbers )
+					DrawPathLabels(Paths, canvas, paint);
+			}
 		}
 
 
@@ -308,66 +260,6 @@ namespace SliceViewer
 
 			ProcessLinearPaths(pathSetIn, drawLabelF);
 		}
-
-
-
-
-
-
-		// zoom
-		private void OnScrollEvent(object o, ScrollEventArgs args)
-		{
-			if (args.Event.Direction == Gdk.ScrollDirection.Up)
-				Zoom *= 1.05f;
-			else
-				Zoom = Math.Max(0.25f, Zoom * (1.0f / 1.05f));
-			QueueDraw();
-		}
-
-
-		// pan support
-		bool left_down = false;
-		Vector2f start_pos = Vector2f.Zero;
-		Vector2f pan_start;
-		private void OnButtonPressEvent(object o, ButtonPressEventArgs args)
-		{
-			if (args.Event.Button == 1) {
-				left_down = true;
-				start_pos = new Vector2f((float)args.Event.X, (float)args.Event.Y);
-				pan_start = Translate;
-			}
-		}
-		private void OnButtonReleaseEvent(object o, ButtonReleaseEventArgs args)
-		{
-			if (left_down)
-				left_down = false;
-		}
-		private void OnMotionNotifyEvent(object o, MotionNotifyEventArgs args)
-		{
-			if (left_down) {
-				Vector2f cur_pos = new Vector2f((float)args.Event.X, (float)args.Event.Y);
-				Vector2f delta = cur_pos - start_pos;
-				delta.y = -delta.y;
-				delta *= 1.0f;  // speed
-				Translate = pan_start + delta / Zoom;
-				QueueDraw();
-			}
-		}
-
-
-		void Reset()
-		{
-			Zoom = 1.0f;
-			Translate = Vector2f.Zero;
-			QueueDraw();
-		}
-
-
-
-
-
-
-
 
 
 	}
